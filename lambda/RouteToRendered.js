28943b19862d1d555275ac52735d1c2f449875c2
Jessie Cris Vicerra <jclvicerra@gmail.com>
2019-05-15 13:08:18 +0800

    * change dockerfile base image to browserless

--- lambda/RouteToRendered.js
+++ lambda/RouteToRendered.js
@@ -1,29 +1,170 @@
 'use strict';
 
+const https = require('https');
+const zlib = require('zlib');
+
+const supportedCompression = ['gzip', 'deflate'];
+
+const postUrl = (options, postData) => new Promise((resolve, reject) => {
+	const req = https.request(options, (distant) => {
+		let response = '';
+		distant.on('data', packet => {
+
+			response += packet.toString();
+		});
+		distant.on('end', () => resolve(response));
+
+	})
+		.on('error', (e) => {
+			reject(e);
+		});
+	req.write(postData);
+	req.end();
+});
+
+const compressBody = (body, compression) => {
+	if (compression === 'gzip') {
+		return zlib.gzipSync(body)
+			.toString('base64');
+	} else if (compression === 'deflate') {
+		return zlib.deflateSync(body)
+			.toString('base64');
+	} else {
+		return body;  // no compression
+	}
+};
+
+const detectCompression = (request) => {
+
+	const accept = request.headers['accept-encoding'] || [];
+	for (const i = 0; i < accept.length; i++) {
+
+		const encoding = `${accept[i].value}`;
+
+		const splitEncoding = encoding.split(',') || [];
+
+		const matchedEncoding = splitEncoding.map((item) => item.trim())
+			.find((compression => supportedCompression.includes(compression)));
+
+		return matchedEncoding || null;
+	}
+	return null;
+};
+
+
 exports.handler = (event, context, callback) => {
 	const request = event.Records[0].cf.request;
 
-	const isFromBot= request.headers['x-prerender-token'] && request.headers['x-prerender-path']
+	const isFromBot = request.headers['x-prerender-token'] && request.headers['x-prerender-path'];
 	if (isFromBot) {
 
-		const encodedPath = encodeURIComponent(`https://${request.headers['x-prerender-path'][0].value}`);
+		// let encodedPath = encodeURIComponent(`https://${request.headers['x-prerender-path'][0].value}`);
+		// encodedPath = encodedPath.replace(/\/+$/, "");
 
-		request.origin = {
-			custom: {
-				domainName: 'prerender.realestate.sg',
-				port: 443,
-				protocol: 'https',
-				readTimeout: 20,
-				keepaliveTimeout: 5,
-				customHeaders: {},
-				sslProtocols: ['TLSv1', 'TLSv1.1'],
-				path: `/?url=${encodedPath}`
-			}
-		};
+		let encodedPath = `https://${request.headers['x-prerender-path'][0].value}`;
+		// const isGzip = hasGzipHeaders(request.headers);
 
-		request.headers['host'] = [{ key: 'Host', value: 'prerender.realestate.sg'}];
-		request['uri'] = "/";
+		// detect compression from request headers
+		const compression = detectCompression(request);
+
+		postUrl({
+			method: 'POST',
+			hostname: 'prerender.realty.com.au',
+			port: 443,
+			path: '/content?token=B19C3CD526B2C',
+			headers: {
+				'Cache-Control': 'no-cache',
+				'Content-Type': 'application/json'
+			},
+		}, JSON.stringify({ url: encodedPath }))
+			.then(async (result) => {
+
+				let response = {
+					status: '200',
+					statusDescription: 'OK',
+					headers: {
+						'x-ssr': [{
+							key: 'X-SSR',
+							value: `Prerender with path ${encodedPath}`
+						}],
+						'cache-control': [{
+							key: 'Cache-Control',
+							value: 'max-age=0',
+						}],
+						'content-type': [{
+							key: 'Content-Type',
+							value: 'text/html; charset=utf-8',
+						}],
+					}
+				};
+
+				try {
+
+					const body = compressBody(result, compression);
+
+					response = {
+						...response,
+						bodyEncoding: compression === null ? 'text' : 'base64',
+						body,
+						headers: {
+							...response.headers,
+							'content-encoding': [{
+								key: 'Content-Encoding',
+								value: compression || 'utf-8',
+							}]
+						}
+					};
+
+					callback(null, response);
+
+					// callback(null, {
+					//     body: `Meta rendering error: ${JSON.stringify(request, null, 2)}`,
+					//     headers: {
+					//         'x-ssr': [{ key: 'X-SSR', value: 'error' }],
+					//     },
+					//     status: 200,
+					//     statusDescription: 'HTTP OK',
+					// });
+
+				} catch (error) {
+
+					// callback(null, {
+					//     ...request,
+					//     headers: {
+					//         ...request.headers,
+					//         'x-ssr': [{
+					//             key: 'X-SSR',
+					//             value: `Prerender Error mode ${compression} with path ${encodedPath}`
+					//         }],
+					//     }
+					// });
+					callback(null, {
+						body: `Meta rendering error: ${error}`,
+						headers: {
+							'x-ssr': [{
+								key: 'X-SSR',
+								value: 'error'
+							}],
+						},
+						status: 200,
+						statusDescription: 'HTTP OK',
+					});
+				}
+
+			})
+			.catch((error) => {
+				callback(null, request);
+
+				// callback(null, {
+				//     body: `Meta rendering error: ${error}`,
+				//     headers: {
+				//         'x-ssr': [{ key: 'X-SSR', value: 'error' }],
+				//     },
+				//     status: 200,
+				//     statusDescription: 'HTTP OK',
+				// });
+			});
+	} else {
+		callback(null, request);
 	}
-
-	callback(null, request);
 };
--- lambda/addRenderHeaders.js
+++ lambda/addRenderHeaders.js
@@ -2,29 +2,29 @@
 
 exports.handler = (event, context, callback) => {
 	const request = event.Records[0].cf.request;
+	const headers = request.headers;
+	const user_agent = headers['user-agent'];
+	const host = headers['host'];
 
-	const isFromBot= request.headers['x-prerender-token'] && request.headers['x-prerender-path']
-	if (isFromBot) {
+	if (user_agent && host) {
+		var prerender = /bot|googlebot|bingbot|yandex|baiduspider|Facebot|facebookexternalhit|twitterbot|rogerbot|linkedinbot|embedly|quora link preview|showyoubot|outbrain|pinterest|slackbot|vkShare|W3C_Validator/i.test(user_agent[0].value);
+		prerender = prerender || /_escaped_fragment_/.test(request.querystring);
+		prerender = prerender && !/\.(js|css|xml|less|png|jpg|jpeg|gif|pdf|doc|txt|ico|rss|zip|mp3|rar|exe|wmv|doc|avi|ppt|mpg|mpeg|tif|wav|mov|psd|ai|xls|mp4|m4a|swf|dat|dmg|iso|flv|m4v|torrent|ttf|woff|svg|eot|woff2)$/i.test(request.uri);
 
-		let encodedPath = encodeURIComponent(`https://${request.headers['x-prerender-path'][0].value}`);
+		const uri = request.uri;
+		const qs = request.querystring;
 
-		encodedPath = encodedPath.replace(/\/+$/, "");
+		const newUri = uri.replace(/\/+$/, "");
 
-		request.origin = {
-			custom: {
-				domainName: 'prerender.realty.com.au',
-				port: 443,
-				protocol: 'https',
-				readTimeout: 20,
-				keepaliveTimeout: 5,
-				customHeaders: {},
-				sslProtocols: ['TLSv1', 'TLSv1.1'],
-				path: `/render?url=${encodedPath}`
-			}
-		};
+		const path = qs ? `${host[0].value}${newUri}?${qs}` : `${host[0].value}${newUri}`;
 
-		request.headers['host'] = [{ key: 'Host', value: 'prerender.realestate.sg'}];
-		request['uri'] = "/";
+		if (prerender) {
+			headers['x-prerender-token'] = [{ key: 'X-Prerender-Token', value: 'PRERENDER_TOKEN' }];
+			headers['x-prerender-host'] = [{ key: 'X-Prerender-Host', value: host[0].value }];
+			headers['x-prerender-path'] = [{ key: 'X-Prerender-Path', value: path }];
+			headers['x-prerender-cachebuster'] = [{ key: 'X-Prerender-Cachebuster', value: Date.now().toString() }];
+		}
+
 	}
 
 	callback(null, request);
